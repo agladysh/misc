74340581d91c469066c7ec47ce45846a8b5f6a54
Alexander Gladysh <agladysh@gmail.com>
2010-10-12 19:37:54 +0400

    bash/remote-add-user: --use-sudo option to invoke sudo remotely

--- bash/remote-add-user.sh
+++ bash/remote-add-user.sh
@@ -7,13 +7,16 @@ GECOS="${2}"
 USERNAME="${3}"
 GROUP="${4}"
 PUBLICKEY="${5}"
-PASSWORDLOGINSTR="${6}"
+
+# TODO: Use getopt.
+OPTION_1="${6}"
+OPTION_2="${7}"
 
 function usage
 {
   echo "Usage:" 2>&1
   echo "" 2>&1
-  echo "  ${0} <host> <username> <userlogin> <usergroup> <key.pub> [--password-login]" 2>&1
+  echo "  ${0} <host> <username> <userlogin> <usergroup> <key.pub> [--password-login] [--use-sudo]" 2>&1
   echo "" 2>&1
   echo "Example:" 2>&1
   echo "  ${0} example.com 'Ivan Ivanov' ivan admin ./ivan_id_rsa.pub" 2>&1
@@ -41,14 +44,38 @@ if [ ! -f "${PUBLICKEY}" ]; then
   usage
 fi
 
-if [ "${PASSWORDLOGINSTR}" == "--password-login" ]; then
+PASSWORD_LOGIN=false
+USE_SUDO=false
+
+# TODO: Use getopt!
+if [ "${OPTION_1}" == "--password-login" ]; then
+  PASSWORD_LOGIN=true
+else
+  if [ "${OPTION_1}" == "--use-sudo" ]; then
+    USE_SUDO=true
+  else
+    if [ ! -z "${OPTION_1}" ]; then
+      usage
+    fi
+  fi
+fi
+
+if [ "${OPTION_2}" == "--password-login" ]; then
+  PASSWORD_LOGIN=true
+else
+  if [ "${OPTION_2}" == "--use-sudo" ]; then
+    USE_SUDO=true
+  else
+    if [ ! -z "${OPTION_2}" ]; then
+      usage
+    fi
+  fi
+fi
+
+if [ "${PASSWORD_LOGIN}" == "true" ]; then
   SSH="ssh -oPubkeyAuthentication=no ${HOST}"
 else
-  if [ ! -z "${PASSWORDLOGINSTR}" ]; then
-    usage
-  else
-    SSH="ssh ${HOST}"
-  fi
+  SSH="ssh ${HOST}"
 fi
 
 COMMAND=" \
@@ -64,4 +91,10 @@ COMMAND=" \
   && chown -R ${USERNAME} ~${USERNAME}/.ssh \
 "
 
-${SSH} "bash -c '${COMMAND}'" < ${PUBLICKEY}
+if [ "${USE_SUDO}" == "true" ]; then
+  COMMAND="sudo bash -c '${COMMAND}'"
+else
+  COMMAND="bash -c '${COMMAND}'"
+fi
+
+${SSH} "${COMMAND}" < ${PUBLICKEY}
